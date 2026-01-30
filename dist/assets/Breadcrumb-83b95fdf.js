e707bc5039cafecc87b6ef7644df6f102b475f97
Marc <81155509+Marc-Industries@users.noreply.github.com>
2026-01-30 22:59:14 +0100

    Add files via upload

---
+++ dist/assets/Breadcrumb-83b95fdf.js
@@ -0,0 +1,11 @@
+import{c as a,u as r,j as e,b as t}from"./index-09130438.js";/**
+ * @license lucide-react v0.469.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=a("ChevronRight",[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]]);/**
+ * @license lucide-react v0.469.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const i=a("House",[["path",{d:"M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8",key:"5wwlr5"}],["path",{d:"M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z",key:"1d0kgt"}]]),o=({items:l=[]})=>{const{t:m}=r();return e.jsx("nav",{className:"flex","aria-label":"Breadcrumb",children:e.jsxs("ol",{className:"inline-flex items-center space-x-1 md:space-x-3",children:[e.jsx("li",{className:"inline-flex items-center",children:e.jsxs(t,{to:"/",className:"inline-flex items-center text-sm font-medium text-white/70 hover:text-[#00D2B4] transition-colors",children:[e.jsx(i,{className:"w-4 h-4 mr-2"}),m({IT:"Home",EN:"Home"})]})}),l.map((s,n)=>e.jsx("li",{children:e.jsxs("div",{className:"flex items-center",children:[e.jsx(c,{className:"w-4 h-4 text-white/40 mx-1"}),s.href?e.jsx(t,{to:s.href,className:"ml-1 text-sm font-medium text-white/70 hover:text-[#00D2B4] transition-colors md:ml-2",children:s.label}):e.jsx("span",{className:"ml-1 text-sm font-medium text-[#00D2B4] md:ml-2",children:s.label})]})},n))]})})};export{o as B};
